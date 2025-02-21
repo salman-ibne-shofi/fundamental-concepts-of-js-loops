79fa7b3f5ac3b36d04139538bed6ac9ce34733fe
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-02-21 17:21:04 +0600

    while loop

---
+++ while.js
@@ -0,0 +1,14 @@
+const number = 7;
+if(number < 10){
+    console.log("choto ekta number paisi");
+}
+console.log("something new is cooking");
+
+// ------------------while -----loop_________________
+let num = 0; // loop variable
+while(num < 5){
+    console.log("loooping", num);
+    // num = num + 1;
+    // num += 1;
+    num++;
+}
