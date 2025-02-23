274ec09c93cbe5b1e9f0aadb023be0c4c0152f87
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-02-24 03:24:14 +0600

    odd and even with while loop task

--- task_while_loop.js
+++ task_while_loop.js
@@ -6,3 +6,19 @@ while (num <= 60) {
 	);
 	num = num + 1;
 }
+
+// task 2
+
+// subtask 1
+let n = 61;
+while (n <= 100){
+    console.log("here is the odd numbers from 61 to 100:",n);
+    n = n + 2;
+}
+
+// subtask 2
+let n2 = 78;
+while (n2 <= 98){
+    console.log("even numbers from 78 to 98:", n2);
+    n2 = n2 + 2;    
+}
