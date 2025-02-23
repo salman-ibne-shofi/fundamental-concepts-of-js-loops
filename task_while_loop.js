7c2fb5086cdd5b485e680f666953e4faba3c083e
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-02-24 03:46:54 +0600

    task 3 sum of odd and even numbers

--- task_while_loop.js
+++ task_while_loop.js
@@ -1,24 +1,48 @@
 // task 1
-let num = 1;
-while (num <= 60) {
-	console.log(
-		"I will invest at least 6 hrs every single day for next 60 days!"
-	);
-	num = num + 1;
-}
+// let num = 1;
+// while (num <= 60) {
+// 	console.log(
+// 		"I will invest at least 6 hrs every single day for next 60 days!"
+// 	);
+// 	num = num + 1;
+// }
 
 // task 2
 
 // subtask 1
-let n = 61;
-while (n <= 100){
-    console.log("here is the odd numbers from 61 to 100:",n);
-    n = n + 2;
-}
+// let num = 61;
+// while (num <= 100){
+//     console.log("here is the odd numbers from 61 to 100:",num);
+//     num = num + 2;
+// }
 
 // subtask 2
-let n2 = 78;
-while (n2 <= 98){
-    console.log("even numbers from 78 to 98:", n2);
-    n2 = n2 + 2;    
-}
+// let num = 78;
+// while (num <= 98){
+//     console.log("even numbers from 78 to 98:", num);
+//     num = num + 2;    
+// }
+
+// task 3
+
+// subtask 1
+// let num = 81;
+// let sum = 0;
+// while (num <= 131){
+//     console.log(num);
+//     num = num + 2;
+//     sum = sum + num;
+// }
+
+// console.log("sum of all odd numbers from 81 to 131: ", sum);
+
+// subtask 2
+// let num = 206;
+// let sum  = 0;
+// while (num <= 311){
+//     console.log(num);
+//     num = num + 2;
+//     sum = sum + num;
+// }
+
+// console.log("sum of even numbers from 206 to 311: ", sum);
